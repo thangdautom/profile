1834d52459f93ddd01b6858b33fd812370fa26c7
Huỳnh Ngọc Thắng <lavang390@gmail.com>
2026-02-02 21:40:06 +0700

    init project

---
+++ src/data/config.ts
@@ -0,0 +1,41 @@
+const config = {
+  title: "Ngoc Thang",
+  description: {
+    long: "Explore the portfolio of Naresh, a full-stack developer and creative technologist specializing in interactive web experiences, 3D animations, and innovative projects. Discover my latest work, including Coding Ducks, The Booking Desk, Ghostchat, and more. Let's build something amazing together!",
+    short:
+      "Discover the portfolio of Naresh, a full-stack developer creating interactive web experiences and innovative projects.",
+  },
+  keywords: [
+    "Ngoc Thang",
+    "portfolio",
+    "full-stack developer",
+    "creative technologist",
+    "web development",
+    "3D animations",
+    "interactive websites",
+    "Coding Ducks",
+    "The Booking Desk",
+    "Ghostchat",
+    "web design",
+    "GSAP",
+    "React",
+    "Next.js",
+    "Spline",
+    "Framer Motion",
+  ],
+  author: "Ngoc Thang",
+  email: "lavang390@gmail.com",
+  site: "https://ngocthang.site",
+
+  get ogImg() {
+    return this.site + "/assets/seo/og-image.png";
+  },
+  social: {
+    twitter: "https://x.com/nothotchaddi",
+    linkedin: "https://www.linkedin.com/in/naresh-khatri/",
+    instagram: "https://www.instagram.com/21.th08/",
+    facebook: "https://www.facebook.com/ngthua218",
+    github: "https://github.com/thangdautom",
+  },
+};
+export { config };
